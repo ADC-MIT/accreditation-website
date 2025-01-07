5b9251fc27d8b37df7d04376f1f063b6a745a405
Abhigyan Trips <contact@abhigyantrips.dev>
2025-01-07 13:00:14 +0530

    chore(deploy): init Dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
 import { withContentCollections } from '@content-collections/next';
 
 /** @type {import('next').NextConfig} */
-const config = {
+const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  async rewrites() {
+    return process.env.NODE_ENV === 'production'
+      ? [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:8000/:path*',
+          },
+        ]
+      : [];
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'helpdesk.alphaspiderman.dev'],
+    },
+  },
 };
 
-export default withContentCollections(config);
+export default withContentCollections(nextConfig);
